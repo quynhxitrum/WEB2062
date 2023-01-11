8f43cb524754c6f8acb0a0c2ca9c7a43984963c2
Quỳnh xì trum <93595501+quynhxitrum@users.noreply.github.com>
2023-01-11 10:51:29 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italin','Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+};
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x,y,z] = arr;
+console.log(x,y,z);
+console.log(arr);
+
+let [main, ,secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[secondary, main] = [main, secondary];
+console.log(main, secondary);
