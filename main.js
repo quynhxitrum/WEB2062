218a4a699fae7bc3fe8629c3fbed688302253a5a
Quỳnh xì trum <93595501+quynhxitrum@users.noreply.github.com>
2023-01-20 19:55:57 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,24 +3,52 @@
 const restaurant = {
     name: 'Classico Italiano',
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
-    categories: ['Italin','Pizzeria', 'Vegetarian', 'Organic'],
+    categories: ['Italin', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0,
+            close: 24,
+        },
+    },
 
-    order: function(starterIndex, mainIndex){
+    order: function (starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[this.mainIndex]];
     },
 };
-const arr = [2,3,4];
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+//chỉ định 1 tên mới
+const { name: restaurantName, openingHours: hours, categories: tags } = restaurant;
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+
+
+
+const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
 const c = arr[2];
 
-const [x,y,z] = arr;
-console.log(x,y,z);
-console.log(arr);
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(arr);
 
-let [main, ,secondary] = restaurant.categories;
+let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
 
 // const temp = main;
@@ -32,15 +60,15 @@ console.log(main, secondary);
 console.log(main, secondary);
 
 // cách nhận 2 giá trị trả về từ 1 hàm
-const[starter, mainCourse] = restaurant.order(2,0);
+const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
 
 // cấu trúc hủy lồng nhau
 const nested = [2, 4, [5, 6]];
 // const [i, j] = nested;
-const [i, ,[j, k]] = nested;
+const [i, , [j, k]] = nested;
 console.log(i, j, k);
 
 // giá trị mặc định
-const [q=1, u=1, y=1] = [8,9];
-console.log(q,u,y);
+const [q = 1, u = 1, y = 1] = [8, 9, 0];
+console.log(q, u, y);
